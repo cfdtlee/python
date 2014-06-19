c1011536c0471ca5e73040529068f96d55725b7b
Eric_in_mac <cfdtlee@gmail.com>
2014-06-20 00:07:54 +0800

    generate is ok

--- gc_server/Generate.py
+++ gc_server/Generate.py
@@ -5,19 +5,36 @@ import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
-def generate_xml(items, tel, features, dishes):
-    doc = Document()  #创建DOM文档对象
-    InfoList = doc.createElement('InfoList') #创建根元素
+
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+from flask import g
+import math
+from xml.dom.minidom import Document
+
+# configuration
+DATABASE = 'gc_data.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+def generate_xml(items):#, tel, features, dishes):
+    
+    doc=Document()  #创建DOM文档对象
+    InfoList = doc.createElement('infoList') #创建根元素
     doc.appendChild(InfoList)
 
-    Title = doc.createElement('Title')
-    Title_text = doc.createTextNode('OptimalList') #元素内容写入
+    Title = doc.createElement('title')
+    Title_text = doc.createTextNode('optimallist') #元素内容写入
     Title.appendChild(Title_text)
     InfoList.appendChild(Title)
 
-    items = (items,"none") ###################
+    #items = (items,"none") ###################
     count = len(items)
-    Count = doc.createElement('Count')
+    Count = doc.createElement('count')
     Count_text = doc.createTextNode(str(count))
     Count.appendChild(Count_text)
     InfoList.appendChild(Count)
@@ -25,67 +42,73 @@ def generate_xml(items, tel, features, dishes):
     for it in items:
         if it == "none":
             break
+        
+        tel = get_tel(it[0])
+        features = get_feature(it[0])
+        dishes = get_dish(it[0])
 
-        Item = doc.createElement('Item')
+        Item = doc.createElement('item')
         InfoList.appendChild(Item)
+        #doc.appendChild(Item)
 
-        ID = doc.createElement('ID')
+        ID = doc.createElement('id')
         ID_text = doc.createTextNode(it[0])
         ID.appendChild(ID_text)
         Item.appendChild(ID)
         
-        Name = doc.createElement('Name')
+        Name = doc.createElement('name')
         Name_text = doc.createTextNode(it[1])
         Name.appendChild(Name_text)
         Item.appendChild(Name)
         
-        Consume = doc.createElement('Consume')
-        Consume_text = doc.createTextNode(str(it[7]))
+        Consume = doc.createElement('consume')
+        Consume_text = doc.createTextNode(str(it[4]))
         Consume.appendChild(Consume_text)
         Item.appendChild(Consume)
         
-        Rate = doc.createElement('Rate')
-        Rate_text = doc.createTextNode(str(it[3]))
+        Rate = doc.createElement('rate')
+        Rate_text = doc.createTextNode(str(it[5]))
         Rate.appendChild(Rate_text)
         Item.appendChild(Rate)
         
-        Taste = doc.createElement('Taste')
-        Taste_text = doc.createTextNode(str(it[4]))
+        Taste = doc.createElement('taste')
+        Taste_text = doc.createTextNode(str(it[6]))
         Taste.appendChild(Taste_text)
         Item.appendChild(Taste)
         
-        Env = doc.createElement('Env')
-        Env_text = doc.createTextNode(str(it[5]))
+        Env = doc.createElement('env')
+        Env_text = doc.createTextNode(str(it[7]))
         Env.appendChild(Env_text)
         Item.appendChild(Env)
         
-        Serv = doc.createElement('Serv')
-        Serv_text = doc.createTextNode(str(it[6]))
+        Serv = doc.createElement('serv')
+        Serv_text = doc.createTextNode(str(it[8]))
         Serv.appendChild(Serv_text)
         Item.appendChild(Serv)
         
-        Lat = doc.createElement('Lat')
-        Lat_text = doc.createTextNode(str(it[12]))
+        Lat = doc.createElement('lat')
+        Lat_text = doc.createTextNode(str(it[16]))
         Lat.appendChild(Lat_text)
         Item.appendChild(Lat)
         
-        Lng = doc.createElement('Lng')
-        Lng_text = doc.createTextNode(str(it[13]))
+        Lng = doc.createElement('lng')
+        Lng_text = doc.createTextNode(str(it[17]))
         Lng.appendChild(Lng_text)
         Item.appendChild(Lng)
         
-        Address = doc.createElement('Address')
-        Address_text = doc.createTextNode(it[11])
+        Address = doc.createElement('address')
+        Address_text = doc.createTextNode(it[10])
         Address.appendChild(Address_text)
         Item.appendChild(Address)
         
-        ItemClass = doc.createElement('ItemClass')
-        ItemClass_text = doc.createTextNode(it[14])
+        ItemClass = doc.createElement('itemclass')
+        ItemClass_text = doc.createTextNode(it[3])
         ItemClass.appendChild(ItemClass_text)
         Item.appendChild(ItemClass)
         
-        Tel = doc.createElement('Tel')
+        Tel = doc.createElement('tel')
         Tel_text = doc.createTextNode(tel[1])
+        #Tel_text = doc.createTextNode(it[20])
         Tel.appendChild(Tel_text)
         Item.appendChild(Tel)
 
@@ -93,8 +116,9 @@ def generate_xml(items, tel, features, dishes):
         for feature in features:
             if feature == 'none':
                 break
-            Feature = doc.createElement('Feature')
+            Feature = doc.createElement('feature')
             Feature_text = doc.createTextNode(feature[1])
+            #Feature_text = doc.createTextNode(it[21])
             Feature.appendChild(Feature_text)
             Item.appendChild(Feature)
 
@@ -102,10 +126,33 @@ def generate_xml(items, tel, features, dishes):
         for dish in dishes:
             if dish == 'none':
                 break
-            Dish = doc.createElement('Dish')
+            Dish = doc.createElement('dish')
             Dish_text = doc.createTextNode(dish[1])
+            #Dish_text = doc.createTextNode(it[22])
             Dish.appendChild(Dish_text)
             Item.appendChild(Dish)
     return doc.toprettyxml()
+
+
+def get_feature(url):
+    cx = sqlite3.connect("gc_data.db")
+    cu = cx.cursor()
+    cu.execute("select * from features where url='"+url+"'")
+    features = cu.fetchall()
+    return features
+
+def get_dish(url):
+    cx = sqlite3.connect("gc_data.db")
+    cu = cx.cursor()
+    cu.execute("select * from great_dishes where url='"+url+"'")
+    dishes = cu.fetchall()
+    return dishes
+
+def get_tel(url):
+    cx = sqlite3.connect("gc_data.db")
+    cu = cx.cursor()
+    cu.execute("select * from tel where url='"+url+"'")
+    tel = cu.fetchone()
+    return tel
 
 
--- gc_server/gc_server.py
+++ gc_server/gc_server.py
@@ -36,8 +36,20 @@ def getdistance(lat1, lng1, lat2, lng2):
     s = s * EARTH_RADIUS
     return s
 
-def getoptscore(): #按照方差公式排序
-    
+def getoptscore(lat, lng, consume, sweet, sour, spicy, salty, order, time, x): #按照方差公式排序
+    open_time = x[9]; #8~22  15位长001111111111111
+    if time <= 8:
+    	time = 8
+    if time >=22:
+    	time = 22
+    istime = (float)(open_time[time-8])
+    distance = getdistance(lat, lng, x[16], x[17])
+    dconsume = float(consume) - float(x[4])
+    dsweet = float(sweet) - float(x[13])
+    dsour = float(sour) - float(x[12])
+    dspicy = float(spicy) - float(x[15])
+    dsalty = float(salty) - float(x[14])
+    score = istime*(distance*distance + dconsume*dconsume + dsweet*dsweet + dsour*dsour + dspicy*dspicy +dsalty*dsalty)
     return score
 
 def find_the_nearest(lat0, lng0):
@@ -45,33 +57,33 @@ def find_the_nearest(lat0, lng0):
     cu = cx.cursor()
     cu.execute("select * from shop")
     item = cu.fetchone() # lat = item[12], lng = item[13]
-    min_dis = getdistance(lat0, lng0, item[17], item[18])
+    min_dis = getdistance(lat0, lng0, item[16], item[17])
     min_item = item
     items = cu.fetchall()
     for it in items:
-        lat = it[17]
-        lng = it[18]
+        lat = it[16]
+        lng = it[17]
         dis = getdistance(lat0, lng0, lat, lng)
         if dis < min_dis:
             min_item = it
             min_dis = dis
     return min_item
 
-def get_the_nearest_3(lat, lng):
+def get_the_nearest(lat, lng, order):
     cx = sqlite3.connect("gc_data.db")
     cu = cx.cursor()
     cu.execute("select * from shop")
     items = cu.fetchall()
     sorted_items = sorted(items, cmp=lambda x,y:cmp(getdistance(lat, lng, x[12], x[13]), getdistance(lat, lng, y[12], y[13])))  ######sorted(L, cmp=lambda x,y:cmp(x[1],y[1]))
-    return sorted_items[0:3]
+    return sorted_items[0+int(order)*10:10+int(order)*10]
 
-def get_the_optimal_10(lat, lng, comsume, sweet, sour, spicy, salty, order):
+def get_the_optimal(lat, lng, consume, sweet, sour, spicy, salty, order, time):
     cx = sqlite3.connect("gc_data.db")
     cu = cx.cursor()
     cu.execute("select * from shop")
     items = cu.fetchall()
-    sorted_items = sorted(items, cmp=lambda x,y:cmp(getoptscore(), getoptscore()))  ######sorted(L, cmp=lambda x,y:cmp(x[1],y[1]))
-    return sorted_items[0:10]
+    sorted_items = sorted(items, cmp=lambda x,y:cmp(getoptscore(lat, lng, consume, sweet, sour, spicy, salty, order, time, x), getoptscore(lat, lng, consume, sweet, sour, spicy, salty, order, time, y)))  ######sorted(L, cmp=lambda x,y:cmp(x[1],y[1]))
+    return sorted_items[0+int(order)*10:10+int(order)*10]
 
 
 def get_feature(url):
@@ -96,31 +108,30 @@ def get_tel(url):
     return tel
 
 
-@app.route('/sort/',methods=['GET'])
-def get_sorted(): #按距离（dis）/综合（opt）排序最近的3个
+@app.route('/opt/',methods=['GET'])
+def get_sorted(): #综合（opt）排序最近的3个
     lat = request.args.get('lat')
     lng = request.args.get('lng')
     how = request.args.get('how')
     lat = float(lat)
     lng = float(lng)
-    if how == 'dis':
-        items = get_the_nearest_3(lat, lng)
-    else:
-        consume = request.args.get('consume')
-        sweet = request.args.get('sweet')
-        sour = request.args.get('sour')
-        spicy = request.args.get('spicy')
-        salty = request.args.get('salty')
-        order = request.args.get('order')
-        items = get_the_optimal_10(lat, lng, comsume, sweet, sour, spicy, salty, order)
-    xm = ''
-    for item in items:
+    consume = request.args.get('consume')
+    sweet = request.args.get('sweet')
+    sour = request.args.get('sour')
+    spicy = request.args.get('spicy')
+    salty = request.args.get('salty')
+    order = request.args.get('order')
+    time = request.args.get('time')
+    items = get_the_optimal(lat, lng, consume, sweet, sour, spicy, salty, order, time)
+    #xm = ''
+    '''for item in items:
         tel = get_tel(item[0])
         features = get_feature(item[0])
         dishes = get_dish(item[0])
+        item = item + tel[1] + features[1] +dishes[1]'''
         #ret = 'Your location is:(%s,%s)' %(lat,lng)
         #ret = ret + 'the nearest shop is' + near_item[0] + str(tel) + str(features) +str(dishes)
-        xm = xm + generate_xml(item, tel, features, dishes)
+    xm = generate_xml(items)
     return str(xm)
  
 @app.route('/loc/',methods=['GET'])
@@ -129,13 +140,9 @@ def data_get():
     lng = request.args.get('lng')
     lat = float(lat)
     lng = float(lng)
-    near_item = find_the_nearest(lat, lng)
-    tel = get_tel(near_item[0])
-    features = get_feature(near_item[0])
-    dishes = get_dish(near_item[0])
-    #ret = 'Your location is:(%s,%s)' %(lat,lng)
-    #ret = ret + 'the nearest shop is' + near_item[0] + str(tel) + str(features) +str(dishes)
-    xm = generate_xml(near_item, tel, features, dishes)
+    order = request.args.get('order')
+    items = get_the_nearest(lat, lng, order)
+    xm = generate_xml(items)
     return str(xm)
 
 @app.route('/data/post/',methods=['POST'])
