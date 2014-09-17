5390e65590c237229e37805b0261c8627b2d5c3d
Eric_in_mac <cfdtlee@gmail.com>
2014-09-17 13:40:54 +0800

    learned how to commit form

--- djcode/mysite/mysite/books/views.py
+++ djcode/mysite/mysite/books/views.py
@@ -5,15 +5,19 @@ from django.http import HttpResponse
 from mysite.books.models import Book
 # Create your views here.
 
-def search_form(request):
-    return render_to_response('search_form.html')
+#def search_form(request):
+#    return render_to_response('search_form.html')
 
 def search(request):
-	if 'q' in request.GET and request.GET['q']:
+	error = False
+	if 'q' in request.GET:
 		#message = 'You searched for: %r' % request.GET['q']
 		q = request.GET['q']
-		books = Book.objects.filter(title__icontains=q)
-		return render_to_response('search_results.html', {'books':books, 'query':q})
-	else:
-		message = 'You submitted an empty form.'
-		return HttpResponse(message)
+		if not q:
+			error = True
+		else:
+			books = Book.objects.filter(title__icontains=q)
+			return render_to_response('search_results.html', {'books': books, 'query': q})
+	return render_to_response('search_form.html', {'error': error})
+		#message = 'You submitted an empty form.'
+		#return HttpResponse(message)
